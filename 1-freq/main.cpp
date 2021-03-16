943e9eba1978064111648d8ae3f57da3b42162fe
wsl debian <54985@etu.he2b.be>
2021-03-16 17:08:37 +0100

    Remove whitespaces

---
+++ 1-freq/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <cstdlib>
+
+void remove_whitespaces(std::fstream fin, std::fstream fout);
+
+int main(int argc, char const *argv[])
+{
+    remove_whitespaces(std::fstream("book.txt"), std::fstream("book_clean.txt"));
+    return 0;
+}
+
+void remove_whitespaces(std::fstream fin, std::fstream fout)
+{
+    std::string str;
+    while (fin >> str)
+    {
+        fout << str;
+    }
+    fin.close();
+    fout.close();
+}
