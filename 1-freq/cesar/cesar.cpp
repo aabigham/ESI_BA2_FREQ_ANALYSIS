be42150a60dfd761ba3a4374e9c57b21610beb5e
wsl debian <54985@etu.he2b.be>
2021-03-22 19:58:32 +0100

    Progress on cesar

--- 1-freq/cesar/cesar.cpp
+++ 1-freq/cesar/cesar.cpp
@@ -1,48 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+
 int main(int argc, char const *argv[])
 {
-    std::cout << "Cesar\n";
-
-    if(argc !=2){
-        std::cout << "One argument needed.\n";
-             exit(1);
-    }
-
-    std::fstream fin;
-    fin.open(argv[1]);
-
-    if (fin.fail())
-      {
-          std::cout << "Input file could not be opened.\n";
-          exit(1);
-      }
-
-
-    char *token = strtok((char *)argv[1], ".txt");
-    char *s_out = strcat(token, "-ciphered.txt");
-
-     std::ofstream fout(s_out);
-     std::string str;
-      fin >> str;
-      int key=5;
-    for (size_t i = 0; i < str.size(); i++)
+    if (argc != 3)
     {
-      
-        if(str.at(i)>='a'&&str.at(i)<='z'){
-            str.at(i)= char(int(str.at(i)+key-97)%26+97);
-        }
-        /* code */
+        std::cout << "Two argument needed.\n";
+        exit(1);
     }
-    
+    else if (strcmp(argv[1], "-c") && strcmp(argv[1], "-u"))
+    {
+        std::cout << "Wrong options, must be \"-c\" to cipher or \"-u\" to uncipher your file.\n";
+        exit(1);
+    }
+    std::fstream fin;
+    fin.open(argv[2]);
+    if (fin.fail())
+    {
+        std::cout << "Input file could not be opened, are you sure it is a text file ?\n";
+        exit(1);
+    }
 
-             fout << str;
+    int key = 5;
+    char *name_fin = strtok((char *)argv[2], ".");
+    char *name_fout;
+    if (!strcmp(argv[1], "-u"))
+    {
+        key = -key;
+        name_fout = strcat(name_fin, "-unciphered.txt");
+    }
+    else
+    {
+        name_fout = strcat(name_fin, "-ciphered.txt");
+    }
+    std::cout << key << " is the key\n";
 
-        fin.close();
-        fout.close();
-        std::cout << s_out << " created\n";
+    std::ofstream fout(name_fout);
+    std::string str;
+    fin >> str;
+    for (size_t i = 0; i < str.size(); i++)
+        if (str.at(i) >= 'a' && str.at(i) <= 'z')
+            str.at(i) = char(int(str.at(i) + key - 97) % 26 + 97);
+    fout << str;
 
+    fin.close();
+    fout.close();
+    std::cout << name_fout << " created\n";
 
     return 0;
 }
