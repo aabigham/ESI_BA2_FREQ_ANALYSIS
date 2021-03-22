3e47ea6f95be3b9887eba92ff373286445c202e7
54247 <54247@etu.he2b.be>
2021-03-22 12:58:56 +0100

    Cesar cipher

--- 1-freq/cesar/cesar.cpp
+++ 1-freq/cesar/cesar.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
-
-void cesar(std::string str);
-
+#include <fstream>
+#include <string.h>
 int main(int argc, char const *argv[])
 {
     std::cout << "Cesar\n";
+
+    if(argc !=2){
+        std::cout << "One argument needed.\n";
+             exit(1);
+    }
+
+    std::fstream fin;
+    fin.open(argv[1]);
+
+    if (fin.fail())
+      {
+          std::cout << "Input file could not be opened.\n";
+          exit(1);
+      }
+
+
+    char *token = strtok((char *)argv[1], ".txt");
+    char *s_out = strcat(token, "-ciphered.txt");
+
+     std::ofstream fout(s_out);
+     std::string str;
+      fin >> str;
+      int key=5;
+    for (size_t i = 0; i < str.size(); i++)
+    {
+      
+        if(str.at(i)>='a'&&str.at(i)<='z'){
+            str.at(i)= char(int(str.at(i)+key-97)%26+97);
+        }
+        /* code */
+    }
+    
+
+             fout << str;
+
+        fin.close();
+        fout.close();
+        std::cout << s_out << " created\n";
+
+
     return 0;
 }
