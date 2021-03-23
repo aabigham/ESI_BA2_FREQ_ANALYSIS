c94bf1aceaf45e4ba0b921129f730ca314d00122
wsl debian <54985@etu.he2b.be>
2021-03-23 23:41:42 +0100

    Add C++17, fix Makefile & README

--- 1_freq/cesar/cesar.cpp
+++ 1_freq/cesar/cesar.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <istream>
 #include <string.h>
 #include <iostream>
 #include <array>
@@ -16,59 +15,62 @@ double getCoincidenceIndex(std::array<size_t, 26> freq, size_t text_len);
 int main(int argc, char const *argv[])
 {
     if (argc != 3)
-    {
+    { // Checks the number or arguments
         std::cerr << "Two argument needed.\n";
         return -1;
     }
     else if (strcmp(argv[1], "-c") && strcmp(argv[1], "-u"))
-    {
+    { // Checks the options
         std::cerr << "Wrong options, must be \"-c\" to cipher or \"-u\" to uncipher your file.\n";
         return -1;
     }
 
-    std::ifstream fin(argv[2], std::ios_base::binary);
+    // Opens the input files in binary and check if it was succesful
+    std::ifstream fin(argv[2]);
     if (fin.fail())
     {
         std::cerr << "Input file could not be opened, are you sure it is a text file ?\n";
         return -1;
     }
+    // Name of input file to re-use for the output file
     char *name_fin = strtok((char *)argv[2], ".");
 
-    char *name_fout;
-    if (!strcmp(argv[1], "-c"))
+    if (!strcmp(argv[1], "-c")) // Ciphering
     {
         int shift = 5;
-        name_fout = strcat(name_fin, "-ciphered.txt");
-        std::ofstream fout(name_fout);
+        char *name_fout = strcat(name_fin, "-ciphered.txt");
+        std::ofstream fout(name_fout); // Opening output file
         for (char c; fin.get(c);)
-        {
+        { // Foreach character, applying the shift
             if (c >= 'a' && c <= 'z')
                 c = char(int(c + shift - 97) % 26 + 97);
-            fout << c;
+            fout << c; // Writing in output file
         }
         fout.close();
     }
-    else if (!strcmp(argv[1], "-u"))
+    else if (!strcmp(argv[1], "-u")) // Unciphering
     {
-        name_fout = strcat(name_fin, "-unciphered.txt");
-        std::ofstream fout(name_fout, std::ios_base::binary);
-
-        const std::array<double, 26> english = {
+        // Frequency of characters in english
+        constexpr std::array<double, 26> english = {
             0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228,
             0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025,
             0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987,
             0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
             0.01974, 0.00074};
 
+        char *name_fout = strcat(name_fin, "-unciphered.txt");
+        std::ofstream fout(name_fout, std::ios_base::binary);
+
+        // Vector that holds the shift value and its chi-squared satistic for the file
         std::vector<std::pair<int, double>> chi_squares;
-        for (int i = 0; i < 26; ++i)
+        for (int i = 0; i < 26; ++i) // Applying every shift possible on the ciphered text file
         {
             int shift = 26 - i;
-            size_t text_len = .0;
-            std::array<size_t, 26> freq;
+            size_t text_len = 0L;
+            std::array<size_t, 26> freq; // Char frequency for the text with current shift
             std::fill_n(freq.begin(), 26, 0);
             for (char c; fin.get(c);)
-            {
+            { // Foreach character, applying the shift and incrementing frequency
                 if (c >= 'a' && c <= 'z')
                 {
                     c = char(int(c + shift - 97) % 26 + 97);
@@ -76,33 +78,30 @@ int main(int argc, char const *argv[])
                     ++freq[int(c) - int('a')];
                 }
             }
-            fin.clear();
+            fin.clear(); // Resetting file head
             fin.seekg(0);
+
+            // Computing chi square for the current frequency for each character
             double chi = .0;
             for (size_t j = 0; j < 26; j++)
-            {
                 chi += (std::pow((freq[j] - (text_len * english[j])), 2) / (text_len * english[j]));
-            }
 
-            chi_squares.push_back(std::make_pair(shift, chi));
+            chi_squares.push_back(std::make_pair(shift, chi / 10000));
         }
-        for (const auto &currPair : chi_squares)
-        {
-            std::cout << "Shift = " << currPair.first << ", Chi = " << currPair.second << std::endl;
-        }
+        for (const auto &[shift, chi] : chi_squares)
+            std::cout << "Shift = " << shift << ", Chi = " << chi << std::endl;
 
         fout.close();
     }
-
     fin.close();
-    std::cout << name_fout << " created\n";
+    std::cout << "File created\n";
     return 0;
 }
 
-double getCoincidenceIndex(std::array<size_t, 26> freq, size_t text_len)
+/*double getCoincidenceIndex(std::array<size_t, 26> freq, size_t text_len)
 {
     double sum = .0;
     for (const auto &n : freq)
         sum += (double(n) / double(text_len)) * ((double(n) - 1) / (double(text_len) - 1));
     return sum;
-}
+}*/
--- README.md
+++ README.md
@@ -23,17 +23,17 @@ Our file structure for this lab looks like this :
 
 ```
 ### Instructions for this lab : 
-In each file you will find a Makefile used for compiling the program. 
+The language we used is C++17 and in each file you will find a Makefile used for compiling the program. 
 
-To compile and run one of these program, open a terminal in the `cesar/` or `vigenere/` folder and then run the `make` command in your terminal. An executable will be created.
+To compile and run one of these program, open a terminal in the `cesar/` or `vigenere/` folder, then run the `make` command in your terminal. An executable will be created.
 
-Running that executable will look like this : 
+Running that executable should be done like this : 
 - `./nameOfExecutable [options] [file.txt]`
 
-The options are :
+Where the options are :
 - `-c` to **cipher** a file. 
 - `-u` to **uncipher** a file. 
 
 Some examples of running these programs : 
 - `./cesar -c book.txt` will create a ciphered version the `book.txt` file, using the cesar method.
-- `./cesar -u book-ciphered.txt` will create a ciphered version the `book-ciphered.txt` file, using the cesar method.
+- `./cesar -u book-ciphered.txt` will ...TODO.
